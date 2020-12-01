0e985b43409d251678838cf7408170a5b79a0f05
liuhe_tech <13132515202@163.com>
2020-12-01 10:43:02 +0800

    添加日志

--- net_monitor.py
+++ net_monitor.py
@@ -8,23 +8,29 @@
 import os
 from playsound import playsound
 import time
+import logging
+import traceback
+
+# 配置日志信息
+logging.basicConfig(filename='net_monitor.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # 设置重连标志位 判断网络是否已重连
 reconnect_flag = False
 
 while True:
     try:
-        # ping 百度网址，查看状态码，如果
+        # ping 百度网址，查看状态码
         state_code = os.system('ping -c 2 www.baidu.com')
         if state_code:
             # 如果断网，则进行语音提示，进行重连，一分钟后重新进行检测
             playsound('duanwangle.mp3')
             # 尝试连接乌班图，重启机器，因为乌班图没有netkeeper，必须重启进入windows
             # 设置连接响应时间为30秒
-            result_code = os.system('ssh -O ConnectTimeout=30 root@192.168.1.x \'sudo reboot\'')
+            result_code = os.system('ssh -O ConnectTimeout=30 liuh@192.168.1.120 \'sudo reboot\'')
             if result_code:
                 # 连接失败
-                playsound('except.mp3')
+                playsound('connect_except.mp3')
             else:
                 # 将重连标记为设为True
                 reconnect_flag = True
@@ -35,4 +41,6 @@ while True:
         # 每隔十秒检测一次
         time.sleep(10)
     except Exception as e:
-        playsound('except.mp3')
+        playsound('shell_except.mp3')
+        # 将错误写入日志
+        logging.debug(traceback.format_exc())
--- test.py
+++ test.py
@@ -7,14 +7,25 @@
 """
 import os
 import time
+import traceback
+import logging
+
+logging.basicConfig(filename='net_monitor.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # 测试ssh响应时间
 # start_time = time.time()
-# state_code = os.system('ssh -o ConnectTimeout=30 liuh@192.168.1.120 \'sudo reboot\'')
+# state_code = os.system('ssh -o ConnectTimeout=30 root@192.168.1.x\'sudo reboot\'')
 # print(state_code)
 # end_time = time.time()
 # print(end_time - start_time)
 
 
 # 测试远程控制windows重启
-# os.system('net rpc shutdown -r -f -I 192.168.1.120 -U administrator%123')
+# os.system('net rpc shutdown -r -f -I 192.168.1.x -U administrator%123')
+
+# 测试日志问题
+# try:
+#     raise Exception('异常')
+# except Exception as e:
+#     logging.debug(traceback.format_exc())
